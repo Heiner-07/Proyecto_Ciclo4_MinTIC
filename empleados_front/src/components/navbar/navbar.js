93dd1cb6df0f49bd664579739f96ab4dd9731c19
Heiner Cufiño <heicufi@gmail.com>
2021-11-08 11:30:09 -0500

    Clase 08112021

--- empleados_front/src/components/navbar/navbar.js
+++ empleados_front/src/components/navbar/navbar.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Dropdown, DropdownButton, Nav, Navbar, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import "./navbar.css";
 
 export default class TopMenu extends React.Component {
     constructor(props) {
@@ -8,14 +11,31 @@ export default class TopMenu extends React.Component {
     }
     render() {
         return (
-            <Navbar bg="primary" variant="dark">
+            <Navbar id="navbar" bg="primary" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#home">Grupo 14 Mision TIC <span id="usuario-sub-branm">i</span> </Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        {/*<Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>*/}
                     </Nav>
+
+                    <DropdownButton id="dropdown-basic-button" title="Usuario">
+                        <Dropdown.Header id="dropdown-header">
+                            <Row>
+                                <FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon>
+                            </Row>
+                            <Row>
+                                USUARIO
+                            </Row>
+                            <Dropdown.Divider>
+                                
+                            </Dropdown.Divider>
+                        </Dropdown.Header>
+                        <Dropdown.Item href="#/action-1">Cerrar sesión</Dropdown.Item>
+                        {/*<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>*/}
+                    </DropdownButton>
                 </Container>
             </Navbar>
         );
